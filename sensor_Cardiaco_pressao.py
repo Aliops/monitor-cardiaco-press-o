af6b6ee33eecbff01db24b01318a6c9f1ba482f9
Aliops <58075214+Aliops@users.noreply.github.com>
2024-02-06 14:52:16 -0300

    Update sensor_Cardiaco_pressao.py

--- sensor_Cardiaco_pressao.py
+++ sensor_Cardiaco_pressao.py
@@ -2,11 +2,11 @@ import sqlite3
 import random
 import time
 
-# Cria uma conexão com o banco de dados SQLite
+
 conn = sqlite3.connect('sensor_data.db')
 c = conn.cursor()
 
-# Cria uma tabela no banco de dados para armazenar os dados do sensor, se ainda não existir
+
 c.execute('''
 CREATE TABLE IF NOT EXISTS health_monitoring (
     timestamp DATETIME,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS health_monitoring (
 )
 ''')
 
-# Simula a geração e inserção de dados do sensor
+
 def generate_and_insert_data():
     timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
     blood_pressure_systolic = random.randint(100, 140)  # Valores de exemplo
@@ -30,7 +30,7 @@ def generate_and_insert_data():
     
     conn.commit()
 
-# Gera e insere dados no banco de dados a cada 10 segundos, simulando dados recebidos do sensor
+
 try:
     while True:
         generate_and_insert_data()
@@ -39,5 +39,4 @@ try:
 except KeyboardInterrupt:
     print("Programa interrompido.")
 
-# Fecha a conexão com o banco de dados ao final
 conn.close()
